fa487a204adb72507b51e8067114d10c68f65458
v siddarth <siddarthgadge4209@gmail.com>
2024-01-29 16:44:58 +0530

    next day

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask
 # create app
 app=Flask(__name__) #entry point __name__
 
-# 47
-
+@app.route('/')
+def home():
+    return "helllo world"
 if __name__ =="__main__":
     app.run()
     
